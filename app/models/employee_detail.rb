c9bc3add1c9af301dba28aacd14205b4e3f3917a
keerthanavinothkumar <vinokeerthu172612@gmail.com>
2022-01-14 18:15:29 +0530

    error fix

--- app/models/employee_detail.rb
+++ app/models/employee_detail.rb
@@ -1,7 +1,7 @@
 class EmployeeDetail < ApplicationRecord
-  belongs_to :employee_details
+  belongs_to :employee_detail, optional: true
   validates :name,  presence: true
-   validates:age,    presence: true
-   validates:gender, presence: true
-   validates:designation, presence: true
+  validates:age,    presence: true
+  validates:gender, presence: true
+  validates:designation, presence: true
 end
